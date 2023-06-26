5c41d51498987337673e020247ace8200306a213
Rain <pyc0422@gmail.com>
2023-06-26 01:17:00 -0500

    fix development

--- jobCompass/src/helpers/fbconfig.ts
+++ jobCompass/src/helpers/fbconfig.ts
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId:import.meta.env.VITE_appId,
   measurementId:import.meta.env.VITE_measurementId
 };
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
