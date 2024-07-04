e60a1916a28c75f9d986f4a98e1caa70e81c12b0
Rain <pyc0422@gmail.com>
2024-07-04 11:58:31 -0500

    minor change

--- jobCompass/src/store/features/jobSlice.ts
+++ jobCompass/src/store/features/jobSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../../store'
 import { Obj, JobType } from '../../helpers/propTypes'
 import { APPLY_STATUS } from '../../const/const'
 // import { Timestamp } from "firebase/firestore";
